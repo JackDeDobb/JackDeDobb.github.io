fbf54239ad09044504fc65fa835df03196a3efde
Jackson DeDobbelaere <jackson.dedobbelaere@c3iot.com>
2020-07-28 20:57:51 -0500

    progress

--- scripts/narrativeviz.js
+++ scripts/narrativeviz.js
@@ -13,50 +13,33 @@ async function init() {
   width = 960 - margin.left - margin.right;
   height = 650 - margin.top - margin.bottom;
 
-  tooltip = d3.select("body")
-  .append("div")
-  .style("opacity", 0)
-  .style("font-size", "16px")
-  .attr("class", "tooltip");
+  tooltip = d3.select('body').append('div')
+                             .style('opacity', 0)
+                             .style('font-size', "16px")
+                             .attr('class', 'tooltip');
 
-  const data = await d3.csv("https://jaceaser.github.io/data.csv").then(d => chart(d, 1960))
+  const data = await d3.csv('https://jaceaser.github.io/data.csv').then(d => chart(d, 1960))
 
   d3.selectAll("img").on("click", handleYearChange);
 }
 
 function chart(csv, filterYear) {
-
   keys = csv.columns.slice(2);
   _csv = csv;
 
-  var year   = [...new Set(csv.map(d => d.year))]
-  var lexemes = [...new Set(csv.map(d => d.lexeme))]
+  var svg = d3.select("#treemap"), margin = {top: 35, left: 90, bottom: 0, right: 15}, width = +svg.attr("width"), height = +svg.attr("height");
 
-  var svg = d3.select("#treemap"),
-  margin = {top: 35, left: 90, bottom: 0, right: 15},
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
+  y = d3.scaleBand().range([margin.top, height - margin.bottom])
+                    .padding(0.1)
+                    .paddingOuter(0.2)
+                    .paddingInner(0.2)
 
-  y = d3.scaleBand()
-  .range([margin.top, height - margin.bottom])
-  .padding(0.1)
-  .paddingOuter(0.2)
-  .paddingInner(0.2)
+  x = d3.scaleLinear().range([margin.left, width - margin.right])
 
-  x = d3.scaleLinear()
-  .range([margin.left, width - margin.right])
+  svg.append("g").attr("transform", `translate(${margin.left},0)`).attr("class", "y-axis");
+  svg.append("g").attr("transform", `translate(0,${margin.top})`).attr("class", "x-axis");
 
-  var yAxis = svg.append("g")
-  .attr("transform", `translate(${margin.left},0)`)
-  .attr("class", "y-axis")
-
-  var xAxis = svg.append("g")
-  .attr("transform", `translate(0,${margin.top})`)
-  .attr("class", "x-axis")
-
-  z = d3.scaleOrdinal()
-  .range(["red", "steelblue"])
-  .domain(keys);
+  z = d3.scaleOrdinal().range(["red", "steelblue"]).domain(keys);
 
   update(filterYear, 0)
 }
@@ -98,29 +81,14 @@ function update(input, speed) {
 
 
   bars.enter().append("rect")
-  .attr("height", y.bandwidth())
-  .merge(bars)
-  .on("mousemove", tooltiphover)
-  .on("mouseout", tooltipleave)
-  .transition().duration(speed)
-  .attr("y", d => y(d.data.lexeme))
-  .attr("x", function (d) {return x(d[0]);})
-  .attr("width", d => x(d[1]) - x(d[0]));
-
-
-  // var text = svg.selectAll(".text")
-  // .data(data, d => d.lexeme);
-
-  // text.exit().remove()
-
-  // text.enter().append("text")
-  // .attr("class", "text")
-  // .attr("text-anchor", "start")
-  // .merge(text)
-  // .transition().duration(speed)
-  // .attr("y", d => y(d.lexeme) + y.bandwidth() / 2)
-  // .attr("x", d => x(d.total) + 5)
-  // .text(d => d.total)
+              .attr("height", y.bandwidth())
+              .merge(bars)
+              .on("mousemove", tooltiphover)
+              .on("mouseout", tooltipleave)
+              .transition().duration(speed)
+              .attr("y", d => y(d.data.lexeme))
+              .attr("x", function (d) {return x(d[0]);})
+              .attr("width", d => x(d[1]) - x(d[0]));
 }
 
 function handleYearChange() {
@@ -144,11 +112,7 @@ function handleYearChange() {
 }
 
 function tooltipleave() {
-  tooltip
-  .transition()
-  .duration(200)
-  .style("opacity", 0);
-
+  tooltip.transition().duration(200).style("opacity", 0);
 }
 
 function tooltiphover(d) {
@@ -156,23 +120,20 @@ function tooltiphover(d) {
   if (d[0] == 0 && d[1] == d.data.Republican) {
     frequency = d.data.Republican;
 
-    tooltip
-    .style("opacity", 1)
-    .style("left", (d3.event.pageX+30) + "px")
-    .style("top", (d3.event.pageY+30) + "px").html("<strong>Frequency: </strong> <span style='color:red'>" + frequency);
+    tooltip.style("opacity", 1)
+           .style("left", (d3.event.pageX+30) + "px")
+           .style("top", (d3.event.pageY+30) + "px").html("<strong>Frequency: </strong> <span style='color:red'>" + frequency);
   } else if (d[0] != 0) {
     frequency = d.data.Democrat;
 
-    tooltip
-    .style("opacity", 1)
-    .style("left", (d3.event.pageX+30) + "px")
-    .style("top", (d3.event.pageY+30) + "px").html("<strong>Frequency: </strong> <span style='color:steelblue'>" + frequency);
+    tooltip.style("opacity", 1)
+           .style("left", (d3.event.pageX+30) + "px")
+           .style("top", (d3.event.pageY+30) + "px").html("<strong>Frequency: </strong> <span style='color:steelblue'>" + frequency);
   } else if (d[0] == 0 && d[1] == d.data.Democrat) {
     frequency = d.data.Democrat;
 
-    tooltip
-    .style("opacity", 1)
-    .style("left", (d3.event.pageX+30) + "px")
-    .style("top", (d3.event.pageY+30) + "px").html("<strong>Frequency: </strong> <span style='color:steelblue'>" + frequency);
+    tooltip.style("opacity", 1)
+           .style("left", (d3.event.pageX+30) + "px")
+           .style("top", (d3.event.pageY+30) + "px").html("<strong>Frequency: </strong> <span style='color:steelblue'>" + frequency);
   }
 }
