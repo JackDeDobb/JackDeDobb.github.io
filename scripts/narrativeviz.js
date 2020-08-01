d43414f03958c060f059e8c0710c887aaabc9e2c
Jackson DeDobbelaere <jackson.dedobbelaere@c3iot.com>
2020-08-01 14:16:39 -0500

    slight

--- scripts/narrativeviz.js
+++ scripts/narrativeviz.js
@@ -209,7 +209,7 @@ function tooltiphover(dataPoint) {
   var htmlString = '<div><strong>' + dataPoint['Player'].slice(0, dataPoint['Player'].indexOf('\\')).replace(/\*/g, '').replace(/\+/g, '') + '</strong></div>';
   htmlString += '-----------------------';
   fieldOrder.forEach(function(field) {
-    var styleRed = (field === currXAxisVariable || field === currYAxisVariable)? ' style="color:red;"' : '';
+    var styleRed = (field === currXAxisVariable || field === currYAxisVariable)? ' style="color:blue;"' : '';
     htmlString += '<div' + styleRed + '><strong>' + uiOutput[field] + ': </strong>' + parseFloat(dataPoint[field].toString()) + '</div>';
   });
 
