32a2f125e09536ef9e9f887b39627f852945c431
Jackson DeDobbelaere <jackson.dedobbelaere@c3.ai>
2021-07-31 17:48:47 -0700

    resolve visualization bug

--- yelpApplication/scripts/flaskr/__init__.py
+++ yelpApplication/scripts/flaskr/__init__.py
@@ -48,14 +48,14 @@ def parseDateTimeFromString(value):
 
 def getInputParameters():
   return {
-    'starRatingMin':  castToIntIfExists(request.args.get('starRatingMin'))  or 1,
-    'starRatingMax':  castToIntIfExists(request.args.get('starRatingMax'))  or 5,
-    'funnyVotesMin':  castToIntIfExists(request.args.get('funnyVotesMin'))  or 0,
-    'funnyVotesMax':  castToIntIfExists(request.args.get('funnyVotesMax'))  or 999999999, # TODO: Flip back to Infinity
-    'coolVotesMin':   castToIntIfExists(request.args.get('coolVotesMin'))   or 0,
-    'coolVotesMax':   castToIntIfExists(request.args.get('coolVotesMax'))   or 999999999, # TODO: Flip back to Infinity
-    'usefulVotesMin': castToIntIfExists(request.args.get('usefulVotesMin')) or 0,
-    'usefulVotesMax': castToIntIfExists(request.args.get('usefulVotesMax')) or 999999999, # TODO: Flip back to Infinity
+    'starRatingMin':        castToIntIfExists(request.args.get('starRatingMin'))  or 1,
+    'starRatingMax':        castToIntIfExists(request.args.get('starRatingMax'))  or 5,
+    'funnyVotesMin':        castToIntIfExists(request.args.get('funnyVotesMin'))  or 0,
+    'funnyVotesMax':        castToIntIfExists(request.args.get('funnyVotesMax'))  or 999999999, # TODO: Flip back to Infinity
+    'coolVotesMin':         castToIntIfExists(request.args.get('coolVotesMin'))   or 0,
+    'coolVotesMax':         castToIntIfExists(request.args.get('coolVotesMax'))   or 999999999, # TODO: Flip back to Infinity
+    'usefulVotesMin':       castToIntIfExists(request.args.get('usefulVotesMin')) or 0,
+    'usefulVotesMax':       castToIntIfExists(request.args.get('usefulVotesMax')) or 999999999, # TODO: Flip back to Infinity
     'dateWrittenMin': parseDateTimeFromString(request.args.get('dateWrittenMin')) or datetime.datetime(2004, 6, 1),
     'dateWrittenMax': parseDateTimeFromString(request.args.get('dateWrittenMax')) or datetime.datetime.now()
   }
@@ -104,14 +104,70 @@ def getDataThatMatchesInputParameters(inputParameters, stopWords):
   return recordsThatMatch
 
 
+def runLDA(dataArrSegment, numberTopics):
+  dataWords = [x['textProcessed'] for x in dataArrSegment]
+  indexToWord = gensim.corpora.Dictionary(dataWords) # Create Dictionary
+  copyDataWords = dataWords
+  corpus = [indexToWord.doc2bow(x) for x in copyDataWords] # Create corpus, Term Document Frequency
+  ldaModel = gensim.models.LdaMulticore(corpus=corpus, id2word=indexToWord, num_topics=numberTopics)
+
+  return [ldaModel, dataWords]
+
+
+def visualizeLDA(ldaModel, dataWords):
+  topics = ldaModel.show_topics(formatted=False)
+  dataWordsFlattened = [word for wordList in dataWords for word in wordList]
+  counter = Counter(dataWordsFlattened)
+
+  out = []
+  for idx, topic in topics:
+    for word, weight in topic:
+        out.append([word, idx, weight, counter[word]])
+
+  df = pd.DataFrame(out, columns=['word', 'topic', 'importance', 'wordCount'])
+  maxWordCount = df['wordCount'].max()
+
+  fig, axes = plt.subplots(3, 3, figsize=(16, 10), sharey=True, dpi=160)
+  colors = [color for _, color in matplotlib.colors.TABLEAU_COLORS.items()]
+  for idx, axis in enumerate(axes.flatten()):
+    axis.set_title('Topic: ' + str(idx + 1), color=colors[idx], fontsize=16)
+    axis.set_ylabel('Word Count', color=colors[idx])
+    axis.tick_params(axis='y', left=False)
+    axis.set_xticklabels(df.loc[df.topic==idx, 'word'], rotation=30, horizontalalignment='right')
+
+    axis.set_ylim(0, 1.2 * maxWordCount)
+    axis.bar(x='word', height='wordCount', data=df.loc[df.topic == idx, :], color=colors[idx], width=0.5, alpha=0.3, label='Word Count')
+    axis.legend(loc='upper left')
+
+    twinAxis = axis.twinx()
+    twinAxis.set_ylim(0, 0.030)
+    twinAxis.bar(x='word', height='importance', data=df.loc[df.topic == idx, :], color=colors[idx], width=0.2, label='Weights')
+    twinAxis.legend(loc='upper right')
+
+  fig.tight_layout(w_pad=2)
+  fig.suptitle('Word Count and Importance of Topic Keywords', fontsize=20, y=1.02)
+
+  return fig
+
+
+def getLDAVisualizationFromDataArr(dataArrSegment, numberTopics):
+  ldaModel, dataWords = runLDA(dataArrSegment, numberTopics)
+  ldaVisualization = visualizeLDA(ldaModel, dataWords)
+
+  return ldaVisualization
+
+
 @app.route('/')
 def runLDAGivenInputParameters():
   # nltk.download('stopwords') # Comment back in, if need to download stopwords
   stopWords = nltk.corpus.stopwords.words('english')
   stopWords.extend(['go', 'get', 'like', 'got', 'us'])
   inputParameters = getInputParameters()
+  numberTopics = 9
 
-  retVal = getDataThatMatchesInputParameters(inputParameters, stopWords)
+  dataArrSegment = getDataThatMatchesInputParameters(inputParameters, stopWords)
+  retVal = getLDAVisualizationFromDataArr(dataArrSegment, numberTopics)
+
 
   return createParsableJSONResponse(retVal)
 
